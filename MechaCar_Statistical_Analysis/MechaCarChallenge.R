6e4a0c0dee6926ff4ca83b1278dc014b7491b327
bwheeler98 <100613516+bwheeler98@users.noreply.github.com>
2022-07-06 15:19:01 -0400

    uploaded technical part of deliverable 2

--- MechaCar_Statistical_Analysis/MechaCarChallenge.R
+++ MechaCar_Statistical_Analysis/MechaCarChallenge.R
@@ -5,4 +5,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 suspension_table <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 total_summary <- suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+total_summary
 
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary
+
+t.test(log10(suspension_table$PSI), mu=mean(log10(suspension_table$PSI)))
